86679b8f4f9d2b026c8d3ab5430e57aca0e70223
Kimani <alex.kimani@student.moringaschool.com>
2024-01-15 12:34:13 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 124;
+let num2 = .5;
+let multiply = num1*num2;
+
+
+let random = Math.floor(Math.random()*1000) +1;
+
+let num3 = 49
+let num4 = 9
+let mod = num3%num4;
+
+
+let max =   Math.max(10,20);
